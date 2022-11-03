26297a0f025698b0cf2c88b8aee998bad8ca3f1e
kos5667 <kos5667@gmail.com>
2022-11-03 16:06:42 +0900

    Room Notifications

--- 01.zoom/src/server.js
+++ 01.zoom/src/server.js
@@ -31,6 +31,14 @@ io.on("connection", (socket) => {
         done();
         socket.to(roomName).emit("welcome");
     })
+
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach(room => socket.to(room).emit("bye"));
+    })
+    socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", msg);
+        done();
+    })
 })
 
 // Web socket
