a1344074ea613c258612ce504b3edbbe233ec53d
kos5667 <kos5667@gmail.com>
2022-11-03 16:56:35 +0900

    Nicknames

--- 01.zoom/src/server.js
+++ 01.zoom/src/server.js
@@ -22,6 +22,7 @@ function onSocketClose() {
 // front에서 보낸 callback function은 backend에서 처리 하는것 보다는 front로 돌려보내 처리
 // backend에서 처리하는 것은 보안상 좋지않음.
 io.on("connection", (socket) => {
+    socket["nickname"] = "Anon";
     socket.onAny((event) => {
         console.log(`Socket Event : ${event}`)
     })
@@ -29,16 +30,17 @@ io.on("connection", (socket) => {
     socket.on("enter_room", (roomName, done) => {
         socket.join(roomName);
         done();
-        socket.to(roomName).emit("welcome");
+        socket.to(roomName).emit("welcome", socket.nickname);
     })
 
     socket.on("disconnecting", () => {
-        socket.rooms.forEach(room => socket.to(room).emit("bye"));
+        socket.rooms.forEach(room => socket.to(room).emit("bye", socket.nickname));
     })
     socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", msg);
+        socket.to(room).emit("new_message", `${socket.nickname} : ${msg}`);
         done();
     })
+    socket.on("nickname", nickname => (socket["nickname"] = nickname))
 })
 
 // Web socket
