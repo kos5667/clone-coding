ee2fa6c2fdd7460440a36e82e99390c1c5f22717
kos5667 <kos5667@gmail.com>
2022-11-04 12:26:18 +0900

    Call Controls

--- 01.zoom/src/server.js
+++ 01.zoom/src/server.js
@@ -13,6 +13,5 @@ app.get("/*", (_, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
-
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);  
