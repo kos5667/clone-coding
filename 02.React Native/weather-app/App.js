b1a2a8643fbc2a429e6ff04ecbd9f65bf7c54c3f
kos5667 <kos5667@gmail.com>
2022-11-07 14:36:02 +0900

    Layout System

--- 02.React Native/weather-app/App.js
+++ 02.React Native/weather-app/App.js
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{flex: 1}}>
+    <View style={{flex: 1, flexDirection: "row"}}>
       <View style={{flex: 1, backgroundColor: "tomato"}}></View>
       <View style={{flex: 1, backgroundColor: "teal"}}></View>
       <View style={{flex: 1, backgroundColor: "orange"}}></View>
