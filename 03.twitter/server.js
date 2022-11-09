3ac35a8bd0fb75f20f32042d755f25db216143b5
kos5667 <kos5667@gmail.com>
2022-11-09 20:43:24 +0900

    Migrating from REST to GraphQL

--- 03.twitter/server.js
+++ 03.twitter/server.js
@@ -1,4 +1,6 @@
 import { ApolloServer, gql } from "apollo-server";
+import fetch from "node-fetch";
+
 let tweets = [
     {
         id: "1",
@@ -31,21 +33,52 @@ const typeDefs = gql`
         username: String!
         firstName: String!
         lastName: String!
+        """
+        Is the sum of firstName + lastName as a string
+        """
         fullName: String!
     }
+    """
+    Tweet object represents a resource for  a Tweet
+    """
     type Tweet {
         id: ID!
         text: String!
         author: User
     }
     type Query {
+        allMovies: [Movie!]!
         allUsers: [User!]!
         allTweets: [Tweet!]!
         tweet(id: ID!) : Tweet
+        movie(id: String!): Movie
     }
     type Mutation {
         postTweet(text: String, userId: ID): Tweet
         deleteTweet(id:ID): Boolean
+    }
+    type Movie {
+        id: Int!
+        url: String!
+        imdb_code: String!
+        title: String!
+        title_english: String!
+        title_long: String!
+        slug: String!
+        year: Int!
+        rating: Float!
+        runtime: Float!
+        genres: [String]!
+        summary: String
+        description_full: String!
+        synopsis: String
+        yt_trailer_code: String!
+        language: String!
+        background_image: String!
+        background_image_original: String!
+        small_cover_image: String!
+        medium_cover_image: String!
+        large_cover_image: String!
     }
 `
 
@@ -59,7 +92,17 @@ const resolvers = {
         },
         allUsers() {
             return users;
-        }
+        },
+        allMovies() {
+        return fetch("https://yts.mx/api/v2/list_movies.json")
+            .then((r) => r.json())
+            .then((json) => json.data.movies);
+        },
+        movie(_, { id }) {
+        return fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+            .then((r) => r.json())
+            .then((json) => json.data.movie);
+        },
     },
     Mutation: {
         postTweet(_, { text, userId }) {
@@ -75,7 +118,8 @@ const resolvers = {
             if (!tweet) return false;
             tweets = tweets.filter(tweet => tweet.id !== id);
             return true;
-        }
+        },
+
     },
     User: {
         fullName({ firstName, lastName}) {
