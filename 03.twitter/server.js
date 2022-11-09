c842862bef16aa16c22779d4d3a9cac3508aee82
kos5667 <kos5667@gmail.com>
2022-11-09 17:04:11 +0900

    Type Resolvers

--- 03.twitter/server.js
+++ 03.twitter/server.js
@@ -1,19 +1,35 @@
 import { ApolloServer, gql } from "apollo-server";
 let tweets = [
     {
-        id:"1",
-        text:"first one",
+        id: "1",
+        text: "first one",
     },
     {
-        id:"2",
-        text:"second one",
+        id: "2",
+        text: "second one",
     },
+]
+
+let users = [
+    {
+        id: "1",
+        firstName: "park",
+        lastName: "don"
+    },
+    {
+        id: "2",
+        firstName: "a",
+        lastName: "aaw"
+    }
 ]
 
 const typeDefs = gql`
     type User {
-        id: ID
-        username: String
+        id: ID!
+        username: String!
+        firstName: String!
+        lastName: String!
+        fullName: String!
     }
     type Tweet {
         id: ID!
@@ -21,6 +37,7 @@ const typeDefs = gql`
         author: User
     }
     type Query {
+        allUsers: [User!]!
         allTweets: [Tweet!]!
         tweet(id: ID!) : Tweet
     }
@@ -38,26 +55,34 @@ const resolvers = {
         tweet(root, { id }) {
             return tweets.find((tweet) => tweet.id === id);
         },
+        allUsers() {
+            return users;
+        }
     },
     Mutation: {
-        postTweet(_, {text, userId}) {
+        postTweet(_, { text, userId }) {
             const newTweet = {
-                id: tweets.length+1,
+                id: tweets.length + 1,
                 text,
             };
             tweets.push(newTweet);
             return newTweet;
         },
-        deleteTweet(_, {id}) {
+        deleteTweet(_, { id }) {
             const tweet = tweets.find(tweet => tweet.id === id);
             if (!tweet) return false;
             tweets = tweets.filter(tweet => tweet.id !== id);
             return true;
         }
+    },
+    User: {
+        fullName(root) {
+            return "hello";
+        },
     }
 }
 
 const server = new ApolloServer({ typeDefs, resolvers })
-server.listen().then(({url}) => {
+server.listen().then(({ url }) => {
     console.log(`Runnig on ${url}`);
 })
